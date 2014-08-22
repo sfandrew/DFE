9a3ec45a601a4c0648064719126623cdcbad472f
Andrew G <andrew@sfrent.net>
2014-08-22 16:54:51 -0700

    Fixing edit on draft

--- app/models/dynamic_forms_engine/dynamic_form_entry.rb
+++ app/models/dynamic_forms_engine/dynamic_form_entry.rb
@@ -116,6 +116,7 @@ module DynamicFormsEngine
       old_properties = self.properties
       new_properties = {}
       old_properties.each_with_index do |(field_id, field_value), index|
+        
         field = DynamicFormField.find(field_id.to_i)
         
         # Prepend "Other: " to options_select_with_other field types
