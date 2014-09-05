da982cf2ce7b6201c92b91b232f40074f50104f5
Andrew G <andrew@sfrent.net>
2014-09-04 18:42:04 -0700

    Added spacer and divider fields

--- app/models/dynamic_forms_engine/dynamic_form_field.rb
+++ app/models/dynamic_forms_engine/dynamic_form_field.rb
@@ -1,7 +1,15 @@
 module DynamicFormsEngine
   class DynamicFormField < ActiveRecord::Base
   	belongs_to :dynamic_form_type
-    validates :name, :field_type, presence: true
+    validates  :field_type, presence: true
+    validates_presence_of :name, if: :divider_spacer_fields
+
+    def divider_spacer_fields
+    	if self.field_type == 'divider' && !self.name.blank?
+    		errors[:attribute] << 'Divider or Spacer cannot contain a name!'
+    		return false
+    	end   
+    end
     
     def attachment?
       field_type.to_s == 'file_upload'
