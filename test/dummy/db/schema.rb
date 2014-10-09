52742776a28455ab09e451f81533d66d165c4d50
Andrew G <andrew@sfrent.net>
2014-10-08 18:28:46 -0700

    Adding field size width

--- test/dummy/db/schema.rb
+++ test/dummy/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141006225835) do
+ActiveRecord::Schema.define(version: 20141008220450) do
 
   create_table "dynamic_forms_engine_dynamic_form_entries", force: true do |t|
     t.integer  "dynamic_form_type_id"
@@ -34,6 +34,7 @@ ActiveRecord::Schema.define(version: 20141006225835) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.boolean  "included_in_report"
+    t.string   "field_width"
   end
 
   create_table "dynamic_forms_engine_dynamic_form_types", force: true do |t|
