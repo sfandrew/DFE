dd7f8306fb630006899df01313d47e3d81506490
Andrew G <andrew@sfrent.net>
2014-10-13 18:18:22 -0700

    Added field type option width

--- app/helpers/dynamic_forms_engine/dynamic_form_entries_helper.rb
+++ app/helpers/dynamic_forms_engine/dynamic_form_entries_helper.rb
@@ -4,11 +4,20 @@ module DynamicFormsEngine
 
     # Loops over fields for this form and renders them
     def render_fields(dynamic_form_entry, builder)
+
       return_html = ""
+      cols = 0
       form_group_exists = false
+
       # raise keys: dynamic_form_entry.errors.keys, ids: dynamic_form_entry.ordered_fields.map(&:id)
-      dynamic_form_entry.dynamic_form_type.ordered_fields.each_with_index do |field,i|
-  
+      dynamic_form_entry.dynamic_form_type.ordered_fields.each_with_index do |field ,i|
+        #Row and width before
+        if cols == 0
+          return_html += '<div class="row">'
+        end
+
+        cols += field.field_width.to_i
+
         # if one field_group already exists, end one fieldset for the start of the next one
         if field.field_type == "field_group"
           if form_group_exists
@@ -17,11 +26,22 @@ module DynamicFormsEngine
           form_group_exists = true
           return_html += ("<fieldset id='step_#{field.id}' class='field-group'><legend>#{field.name.humanize}</legend>")
         end
+
         errors = dynamic_form_entry.errors.full_messages_for(field.name.to_sym)
         return_html += render_field_with_value(field,errors,builder,dynamic_form_entry)
+        #closes the div if the next field is greater than 12
+        if !dynamic_form_entry.dynamic_form_type.ordered_fields[i+1].nil? && (cols + dynamic_form_entry.dynamic_form_type.ordered_fields[i+1].field_width.to_i > 12) 
+          return_html += "</div><div class='clear spacer'></div>"
+          cols = 0
+        end
+        #add space between last field and submit button
+        return_html << "<div class='clear spacer'></div>" if dynamic_form_entry.dynamic_form_type.ordered_fields[i+1].nil?
+
       end
+
       return_html += "</fieldset>" if form_group_exists #final closing fieldset tag
       return_html.html_safe
+
     end
 
     # Handles logic and rendering of individual field when creating dynamic form entries
