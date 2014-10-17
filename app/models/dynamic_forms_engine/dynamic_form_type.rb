645c97f69ceaad94108ffebfc9af20691c7c280e
Andrew G <andrew@sfrent.net>
2014-10-17 15:26:15 -0700

    Added public form validation

--- app/models/dynamic_forms_engine/dynamic_form_type.rb
+++ app/models/dynamic_forms_engine/dynamic_form_type.rb
@@ -8,9 +8,18 @@ module DynamicFormsEngine
     validates :name, :description, :fields, presence: true
     validates :form_type, presence: true, :inclusion => { :in => %w(Default-form Multi-step), 
                                                       :message => "%{value} is not a valid choice" }
-    validates :public, :inclusion => {:in => [true, false], :message => 'That is not a valid choice!'}
-    validate :field_group_requirement, :other_option_validate, :field_group_order
+    validates :is_public, :inclusion => {:in => [true,false], :message => 'That is not a valid choice!'}
+    validate :field_group_requirement, :other_option_validate, :field_group_order, :public_form
 
+    def public_form
+      if self.is_public
+        self.fields.each do |check_field|
+          if check_field.field_type == "contacts" || check_field.field_type == "file_upload"
+            errors.add check_field.name,  "A public form cannot have a Contacts or file upload field"
+          end
+        end
+      end
+    end
 
     def field_group_order
       self.fields.each_with_index do |item, index|
