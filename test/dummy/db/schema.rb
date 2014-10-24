e0c3e19e0f3848fa06aa9aa5cfb0deb083726007
Max Kaplan <max@sfrent.net>
2014-10-24 14:03:55 -0700

    latest

--- test/dummy/db/schema.rb
+++ test/dummy/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141024180114) do
+ActiveRecord::Schema.define(version: 20141024205467) do
 
   create_table "dynamic_forms_engine_dynamic_form_entries", force: true do |t|
     t.integer  "dynamic_form_type_id"
