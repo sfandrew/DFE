c5c997082adedd740b61823a1f920a8771298865
Andrew G <andrew@sfrent.net>
2014-11-04 16:54:28 -0800

    Fixed file upload on form draft bug

--- app/models/dynamic_forms_engine/dynamic_form_entry.rb
+++ app/models/dynamic_forms_engine/dynamic_form_entry.rb
@@ -104,13 +104,13 @@ module DynamicFormsEngine
       end
     end
 
-
     # Properties are initially saved as {"<field_id>" => "<field_value>"} 
     # However, relying on the original field object is bad because it might change
     #  This function transmutes the properties to a form that doesn't rely on the original field object
     # Should be run as before_create filter
-    def format_properties
+     def format_properties
       old_properties = self.properties
+      old_entry = DynamicFormEntry.find(self.id) if !self.new_record?
       new_properties = {}
       old_properties.each_with_index do |(field_id, field_value), index|
         field = DynamicFormField.find(field_id.to_i)
@@ -120,6 +120,15 @@ module DynamicFormsEngine
           field_value = "Other: " + field_value
         end
         new_properties[index] = {name: field.name, type: field.field_type, value: field_value, id: field_id}
+      end
+      # this re-submits the file upload file without the user to re-submit the file again
+      if old_entry
+        old_entry.each_field_with_value do |index_val, field|
+          if field[:type] == "file_upload" && !old_properties.has_key?(field[:id])
+            last_property = new_properties.size
+            new_properties[last_property] = { name: field[:name], type: field[:type], value: field[:value], id: field[:id] } 
+          end
+        end
       end
       self.properties = new_properties
     end
