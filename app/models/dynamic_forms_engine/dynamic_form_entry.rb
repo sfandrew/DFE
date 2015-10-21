99c751f0c0d9f2311a2acb18bc6d110dc7312fc8
Andrew G <andrew@sfrent.net>
2015-10-20 18:17:27 -0700

    UI fixes to calendar and phone fieid

--- app/models/dynamic_forms_engine/dynamic_form_entry.rb
+++ app/models/dynamic_forms_engine/dynamic_form_entry.rb
@@ -42,6 +42,7 @@ module DynamicFormsEngine
 
     # http://stackoverflow.com/questions/8634139/phone-validation-regex
     def valid_phone?(field_value)
+      field_value.gsub!(/[^0-9]/, '')
       !(field_value =~ /(?:(?:\+?1\s*(?:[.-]\s*)?)?(?:(\s*([2-9]1[02-9]|[2-9][02-8]1|[2-9][02-8][02-9]‌​)\s*)|([2-9]1[02-9]|[2-9][02-8]1|[2-9][02-8][02-9]))\s*(?:[.-]\s*)?)([2-9]1[02-9]‌​|[2-9][02-9]1|[2-9][02-9]{2})\s*(?:[.-]\s*)?([0-9]{4})\s*(?:\s*(?:#|x\.?|ext\.?|extension)\s*(\d+)\s*)?$/).nil?
     end
 
