9ba96f6546772d2a380a8d0e6481bfa7d14a9599
Andrew G <andrew@sfrent.net>
2016-01-14 12:39:31 -0800

    Tenant address

--- app/mailers/dynamic_forms_engine/form_entry_mailer.rb
+++ app/mailers/dynamic_forms_engine/form_entry_mailer.rb
@@ -7,7 +7,7 @@ module DynamicFormsEngine
 	  		if Rails.env.development?
 	  			@domain_name = "http://localhost:3000"
 	  		else
-	  			@domain_name = "https://tenant-application.herokuapp.com"
+	  			@domain_name = "https://a.tenant.today"
 	  		end
 	  		mail(to: "#{form_entry.user.email}", subject: form_entry.in_progress ? 'Your Applicaton has been saved' : 'Your Application has been submitted')
 	  	end
